6bb2b0f6ae06f449c3a355029120fad9f627cecf
Giga23 <dy189@scarletmail.rutgers.edu>
2020-02-13 23:03:41 -0500

    added populate method and Node class

---
+++ HW1/src/hw1/Node.java
@@ -0,0 +1,40 @@
+package hw1;
+
+public class Node {
+
+	public int x;
+	public int y;
+	public int move;
+	public boolean visited;
+	
+	public Node() {
+		this.x = 0;
+		this.y = 0;
+		this.move = 0;
+		this.visited = false;
+	}
+	
+	public Node(int x, int y, int move){
+		this.x = x;
+		this.y = y;
+		this.move = move;
+		this.visited = false;
+	}
+	
+	public int getX() {
+		return this.x;
+	}
+	
+	public int getY() {
+		return this.y;
+	} 
+	
+	public int getMove() {
+		return this.move;
+	}
+	
+	public boolean getVisited() {
+		return this.visited; 
+	}
+	
+}
--- HW1/src/hw1/main.java
+++ HW1/src/hw1/main.java
@@ -1,6 +1,45 @@
 package hw1;
 
 public class main {
-	//call methods for each task here
 	
+	public static Node grid[][] = new Node[5][5]; 
+	public static int n = 5;
+	
+	public static void populate() {
+		for(int i = 0; i < 5; i++) {
+			for(int j = 0; j < 5; j++) {
+				boolean valid = false;
+				int move = 0;
+				if(i == 0 && j == 0) {
+					grid[0][0] = new Node(0,0,0);
+					break;
+				}
+				if(i == n-1 && j == n-1)
+					break;
+				while (!valid) {
+					int count = 0;
+					move = (int) (Math.random() * 5); 
+					if (i + move < n)
+						count++;
+					if (j + move < n)
+						count++;
+					if (i - move >= 0)
+						count++;
+					if (i - move >= 0)
+						count++;
+					if (count > 0)
+						valid = true; 
+				}
+				grid[i][j] = new Node(i,j,move);
+			}
+		}
+	}
+	
+	//call methods for each task here
+	public static void main(String args[]) {
+		
+		populate();
+		
+		
+	}
 }
