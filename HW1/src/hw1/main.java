4aaac10a4fab5e5d3c1399ba2e4779bb061e723f
Giga23 <dy189@scarletmail.rutgers.edu>
2020-02-14 01:42:57 -0500

    created evaluate method

--- HW1/src/hw1/main.java
+++ HW1/src/hw1/main.java
@@ -1,8 +1,13 @@
 package hw1;
 
+import java.util.LinkedList;
+import java.util.Queue;
+
 public class main {
 	
 	public static Node grid[][] = new Node[5][5]; 
+	public static int solution[][] = new int[5][5];
+	
 	public static int n = 5;
 	
 	public static void populate() {
@@ -35,11 +40,53 @@ public class main {
 		}
 	}
 	
+	public void evaluate() {
+		Queue<Node> q = new LinkedList<Node>();
+		q.add(grid[0][0]);
+		int count = 0;
+		solution[0][0] = count;
+		
+		while(!q.isEmpty()) {
+			Node cell = q.remove();
+			int x = cell.x;
+			int y = cell.y; 
+			int move = cell.move;
+			
+			count++;
+			if(x == n && y == n)
+			{
+				solution[5][5] = count;
+				return;
+			}
+			
+			if(count == n * n)
+				return;
+			
+			if(!cell.visited) {
+				cell.visited = true;
+				solution[x][y] = count;
+				if(x + move < n)
+					q.add(grid[x+move][y]);
+				if(y + move < n) 
+					q.add(grid[x][y+move]);
+				if(x - move >= 0) 
+					q.add(grid[x-move][y]);
+				if(y - move >= 0) 
+					q.add(grid[x][y-move]);
+			}
+			else if(count < solution[x][y]) {
+				solution[x][y] = count;
+			}
+		}
+	}
+	
 	//call methods for each task here
 	public static void main(String args[]) {
-		
 		populate();
-		
-		
+		for (int i = 0; i < 5; i++) {
+			for(int j = 0; j < 5; j++)
+				solution[i][j] = -1;
+		}
 	}
+	
 }
